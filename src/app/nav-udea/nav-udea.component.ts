3d968ff6b512c91930d962e1ba50adae20cefde6
santiagoalex <santiagos9511@gmail.com>
2018-11-28 00:04:21 -0500

    las funcionalidades full pero la busqueda es simple

--- src/app/nav-udea/nav-udea.component.ts
+++ src/app/nav-udea/nav-udea.component.ts
@@ -3,6 +3,8 @@ import { BreakpointObserver, Breakpoints, BreakpointState } from '@angular/cdk/l
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { google } from '@agm/core/services/google-maps-types';
+import { Nivel } from '../nivel'
+import { PisosService } from '../pisos.service'
 import {NgbModal, ModalDismissReasons} from '@ng-bootstrap/ng-bootstrap';
 
 var historicalOverlay;
@@ -16,7 +18,8 @@ var historicalOverlay;
 })
 export class NavUdeaComponent {
   
-
+  public searching: boolean = false;
+  public niveles : Nivel[];
     title: string = 'GeoMapsUdeA';
     lat1: number = 6.2676;
     lng1: number = -75.569;
@@ -24,12 +27,31 @@ export class NavUdeaComponent {
     lat:any;
     lng:any;
 
+
+    ngOnInit() {
+      this.getPisos();
+    }
+
+
+    getPisos(): void {
+      this.pisosService.getPisos().subscribe(niveles => this.niveles=niveles);
+   }
+
+
+    public showSearchResults(event: any): void {
+      if (event.target.value.length >= 3) {
+        this.searching = true;
+      } else {
+        this.searching = false;
+      }
+    }
+
   isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
     .pipe(
       map(result => result.matches)
     );
     closeResult: string;  
-  constructor(private breakpointObserver: BreakpointObserver, private modalService: NgbModal) {
+  constructor(private pisosService: PisosService,private breakpointObserver: BreakpointObserver, private modalService: NgbModal) {
     if (navigator)
     {
     navigator.geolocation.getCurrentPosition( pos => {
@@ -56,6 +78,15 @@ export class NavUdeaComponent {
   }
   open3(content3:any) {
     this.modalService.open(content3, {ariaLabelledBy: 'modal-basic-title3'}).result.then((result) => {
+      this.closeResult = `Closed with: ${result}`;
+    }, (reason) => {
+      this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
+    });
+  }
+
+
+  open4(content4:any) {
+    this.modalService.open(content4, {ariaLabelledBy: 'modal-basic-title4'}).result.then((result) => {
       this.closeResult = `Closed with: ${result}`;
     }, (reason) => {
       this.closeResult = `Dismissed ${this.getDismissReason(reason)}`;
